ee3f9ef4411e773cd2cfd18395f1f7f080510a4f
Le Vo Thanh Hong <honglvt.gg@gmail.com>
2024-10-24 22:22:34 +0700

    --7

--- actions/actions.py
+++ actions/actions.py
@@ -1,4 +1,4 @@
-# This files contains your custom actions which can be used to run
+﻿# This files contains your custom actions which can be used to run
 # custom Python code.
 #
 # See this guide on how to implement these action:
@@ -25,3 +25,18 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+class ActionSetUserMessage(Action):
+    def name(self) -> Text:
+        return "action_set_slot_ly_do_khach_hang_khong_tra_no"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        last_message = tracker.latest_message.get('text')
+        return [SlotSet("slot_ly_do_khach_hang_khong_tra_no", last_message)]
